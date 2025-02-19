ed1984d38b51756f31e6983c4dada02b24527046
Alex <alex@sailvisionpro.com>
2025-02-19 15:57:34 -0800

    Add initial UI and sample data

--- PopAI/PopAIApp.swift
+++ PopAI/PopAIApp.swift
@@ -1,10 +1,20 @@
 import SwiftUI
 
+enum NMEASource {
+    case TCP
+    case SampleData
+}
+
+enum Unit {
+    case Metric
+    case USCS
+}
+
 @main
 struct PopAIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ConversationView()
         }
     }
 }
---
+++ PopAI/Views.swift
@@ -0,0 +1,99 @@
+import SwiftUI
+
+struct ConversationView: View {
+    @State private var conversation: String = """
+PopAI, what is the current depth?
+
+2.1 feet
+"""
+    @State private var listening: Bool = false
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text(conversation)
+                    .font(.largeTitle)
+                    .padding()
+                    .frame(
+                        maxWidth: .infinity, maxHeight: .infinity,
+                        alignment: .topLeading)
+                HStack {
+                    Button(action: {
+                        listening = !listening
+                    }) {
+                        Text((listening ? "Stop" : "Start") + " Listening")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(
+                                listening ? Color.secondary : Color.blue
+                            )
+                            .foregroundColor(Color.white)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                    }
+                    NavigationLink(destination: SettingsView()) {
+                        Image(systemName: "gearshape.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .clipShape(Circle())
+                            .foregroundColor(Color.gray)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+            }
+        }
+    }
+}
+
+struct SettingsView: View {
+    @State private var enableLogging: Bool = true
+    @State private var nmeaAddress: String = "192.168.4.1:1456"
+    @State private var nmeaSource: NMEASource = NMEASource.TCP
+    @State private var draftUnit: Unit = Unit.USCS
+
+    var body: some View {
+        Form {
+            Section(header: Text("Units")) {
+                Picker("Draft", selection: $draftUnit) {
+                    Text("Metric").tag(Unit.Metric)
+                    Text("USCS (\"Imperial\")").tag(Unit.USCS)
+                }
+            }
+            Section(header: Text("NMEA")) {
+                Picker("Data Source", selection: $nmeaSource) {
+                    Text("Remote TCP").tag(NMEASource.TCP)
+                    Text("Sample Data").tag(NMEASource.SampleData)
+                }
+                TextField("Address", text: $nmeaAddress)
+                    .disableAutocorrection(true)
+                    .disabled(nmeaSource == NMEASource.SampleData)
+                    .foregroundStyle(
+                        nmeaSource == NMEASource.TCP ? .primary : .secondary)
+            }
+            Section(header: Text("Logging")) {
+                Toggle("Enable", isOn: $enableLogging)
+                NavigationLink(destination: LogsView()) {
+                    Text("View log")
+                }.disabled(!enableLogging)
+            }
+        }
+        .navigationTitle("Settings")
+    }
+}
+
+struct LogsView: View {
+    @State private var logs: String = NMEA.sampleData
+    @State private var expandAIS: Bool = false
+
+    var body: some View {
+        ScrollView([.horizontal, .vertical]) {
+            Text(logs)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    ConversationView()
+}
